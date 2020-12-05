785c2fdcc6c42c01a857aec96b626147c66128f4
ryan <ryan.klintworth@gmail.com>
2020-12-05 15:20:45 -0600

    created a working modal

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 $(() => {
   // - code here
-
   $('form').on('submit', (event) => {
     event.preventDefault();
     const userInput = $('input[type="text"]').val();
     let pairedWines = $(event.target).attr('id')
+    // - building out the modal
+    // - grabbing modal and close button elements
+    const $openModal = $('button');
+    const $modal = $('.modal');
+    const $closeModal = $('.close');
+
     $.ajax({
       url: `https://api.spoonacular.com/food/wine/pairing?food=${userInput}&apiKey=8d08e6b62869469d8ed4ab6498d5f3ae`,
     }).then(
       (data) => {
         console.log(data);
-        let $error = $('<div>').text(data.message).appendTo('dl')
-      $('.wines')
-        .html(data.pairedWines);
+
+      const openModal = () => {
+        $modal.css('display', 'block');
+      }
+
+      const closeModal = () => {
+        $modal.css('display', 'none')
+      }
+
+      $openModal.on('click', openModal);
+      $closeModal.on('click', closeModal);
+
+      // - setting an on click event to open the modal
+      // $('button').on('click', () => {
+      //   $('modal').style.display = 'flex';
+      // })
+      // console.log($closeModal);
+      //
+      // // - setting an on click event to close the modal
+      // $('.close').on('click', () => {
+      //   $('.modal').style.display = 'none';
+      // })
+      // let $error = $('<div>').text(data.message).appendTo('dl')
+      // $('.wines')
+      //   .html(data.pairedWines['0'] + ' ' + data.pairedWines['1'] + ' ' +  data.pairedWines['2']);
       $('.text')
         .html(data.pairingText);
       $('.title')
