0242f32349e3c92fca1837c1213ae7eb227c7b06
ryan <ryan.klintworth@gmail.com>
2020-12-06 17:49:48 -0600

    modal is 95% functional

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ $(() => {
     event.preventDefault();
     const userInput = $('input[type="text"]').val();
     let pairedWines = $(event.target).attr('id')
+
     // - building out the modal
     // - grabbing modal and close button elements
-    const $openModal = $('button');
+    const $openModal = $('#openModal');
     const $modal = $('.modal');
     const $closeModal = $('.close');
 
+    // - grabbing wine pairing information from www.spoonacular.com API
     $.ajax({
       url: `https://api.spoonacular.com/food/wine/pairing?food=${userInput}&apiKey=8d08e6b62869469d8ed4ab6498d5f3ae`,
     }).then(
       (data) => {
         console.log(data);
 
+      // - Event handlers
       const openModal = () => {
         $modal.css('display', 'block');
       }
@@ -24,22 +27,17 @@ $(() => {
         $modal.css('display', 'none')
       }
 
+      // - Event listeners
+      // - setting an on click event to open/close the modal
       $openModal.on('click', openModal);
       $closeModal.on('click', closeModal);
 
-      // - setting an on click event to open the modal
-      // $('button').on('click', () => {
-      //   $('modal').style.display = 'flex';
-      // })
-      // console.log($closeModal);
-      //
-      // // - setting an on click event to close the modal
-      // $('.close').on('click', () => {
-      //   $('.modal').style.display = 'none';
-      // })
+      // - if someone types in something not in the API, error data will display
       // let $error = $('<div>').text(data.message).appendTo('dl')
       // $('.wines')
+
       //   .html(data.pairedWines['0'] + ' ' + data.pairedWines['1'] + ' ' +  data.pairedWines['2']);
+
       $('.text')
         .html(data.pairingText);
       $('.title')
@@ -55,6 +53,7 @@ $(() => {
         .text('link')
         .attr('target', '_blank');
       },
+      // - log any errors to the page
       (error) => {
       console.log('hello');
       }
