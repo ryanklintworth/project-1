3cd2a60b5f7f7cd68fd421f406f351a06a1ab1e6
ryan <ryan.klintworth@gmail.com>
2020-12-07 22:51:03 -0600

    finalized functionality and styling

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $(() => {
   // - code here
+
 	// - building out the modal
 	// - grabbing modal and close button elements
 	const $modal = $('.modal');
 	const $closeModal = $('.close');
 	// - event handlers
 	const closeModal = () => {
-		$modal.css('display', 'none')
+		$modal.css('display', 'none');
 		// - initialize values to empty strings
-    // - this resets the modal so that an error response doesn't show on top of other information
+    // - I've found this to reset the modal so that an error response doesn't show on top of other information
 		$('.status').html('');
 		$('.text').html('');
 		$('.title').html('');
@@ -16,6 +17,7 @@ $(() => {
 		$('.imageUrl').attr('src', '');
 		$('.price').html('');
 		$('.link').attr('href','').text('');
+    $('#food').val('');
 	}
 	// - event listeners
 	// - setting an on click event to close the modal
@@ -24,14 +26,15 @@ $(() => {
   	$('form').on('submit', (event) => {
     	event.preventDefault();
     	const userInput = $('input[type="text"]').val();
-    	let pairedWines = $(event.target).attr('id')
+    	let pairedWines = $(event.target).attr('id');
+      $('#preLoader').css('display', 'block'); // - create a preLoader
 		// - grabbing wine pairing information from www.spoonacular.com API
 		$.ajax({
 		  	url: `https://api.spoonacular.com/food/wine/pairing?food=${userInput}&apiKey=8d08e6b62869469d8ed4ab6498d5f3ae`,
 		}).then(
 			(data) => {
         console.log(data);
-        // - if/else statment to show error information inside the modal if API does not have wine information for use input
+        // - if/else statement to show error information inside the modal if API does not have wine information for use input
 				if ('status' in data) {
 					if (data.status == 'failure') {
 						$('.status').html(data.message).css('display', 'block'); // - show status div
@@ -40,6 +43,7 @@ $(() => {
 					}
 				}
 				else {
+          $('#preLoader').css('display', 'none'); // - close preLoader
 					$('.status').html('').css('display', 'none'); // - hide status div
 					$('.text').html(data.pairingText);
 					$('.title').html(data.productMatches['0']['title']);
@@ -47,8 +51,17 @@ $(() => {
 					$('.imageUrl').attr('src', data.productMatches['0']['imageUrl']);
 					$('.price').html(data.productMatches['0']['price']);
 					$('.link').attr('href', data.productMatches['0']['link']).text('Buy Your Own Bottle').attr('target', '_blank');
+          // - local storage of information given to the user
+          localStorage.setItem("paired wines", data.pairedWines['0']);
+          localStorage.setItem("pairing text", data.pairingText);
+          localStorage.setItem("title", data.productMatches['0']['title']);
+          localStorage.setItem("description", data.productMatches['0']['description']);
+          localStorage.setItem("image", data.productMatches['0']['imageUrl']);
+          localStorage.setItem("price", data.productMatches['0']['price']);
+          localStorage.setItem("link", data.productMatches['0']['link']);
+          console.log(localStorage);
 				}
-				$modal.css('display', 'block'); //update display to block AFTER updating the text.
+				$modal.css('display', 'block');// - update display to block AFTER updating the text.
 			},
 			// - log any errors to the page
 			(error) => {
