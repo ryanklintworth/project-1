8dfbf79ac5083bb61a1e0405f57197435a8a4d43
ryan <ryan.klintworth@gmail.com>
2020-12-08 21:33:26 -0600

    final adjustments

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ $(() => {
 	// - grabbing modal and close button elements
 	const $modal = $('.modal');
 	const $closeModal = $('.close');
+
 	// - event handlers
 	const closeModal = () => {
 		$modal.css('display', 'none');
+
 		// - initialize values to empty strings
     // - I've found this to reset the modal so that an error response doesn't show on top of other information
 		$('.status').html('');
@@ -22,19 +24,24 @@ $(() => {
 	// - event listeners
 	// - setting an on click event to close the modal
 	$closeModal.on('click', closeModal);
+
 	// - on submit event listener for the form
+  // - this allows the user to use the AJAX request
   	$('form').on('submit', (event) => {
+      // - the event continues to propogate as usual
     	event.preventDefault();
     	const userInput = $('input[type="text"]').val();
     	let pairedWines = $(event.target).attr('id');
       $('#preLoader').css('display', 'block'); // - create a preLoader
+
 		// - grabbing wine pairing information from www.spoonacular.com API
 		$.ajax({
 		  	url: `https://api.spoonacular.com/food/wine/pairing?food=${userInput}&apiKey=8d08e6b62869469d8ed4ab6498d5f3ae`,
 		}).then(
 			(data) => {
         console.log(data);
-        // - if/else statement to show error information inside the modal if API does not have wine information for use input
+
+        // - if/else statement to show error information inside the modal if API does not have wine information for user input
 				if ('status' in data) {
 					if (data.status == 'failure') {
 						$('.status').html(data.message).css('display', 'block'); // - show status div
@@ -43,14 +50,19 @@ $(() => {
 					}
 				}
 				else {
+          
+          // - the preLoader and information displays after userInput
           $('#preLoader').css('display', 'none'); // - close preLoader
-					$('.status').html('').css('display', 'none'); // - hide status div
+					$('.status').html('').css('display', 'none'); // - hide error div
+
+          // - userInput populates the different objects below
 					$('.text').html(data.pairingText);
 					$('.title').html(data.productMatches['0']['title']);
 					$('.description').html(data.productMatches['0']['description']);
 					$('.imageUrl').attr('src', data.productMatches['0']['imageUrl']);
 					$('.price').html(data.productMatches['0']['price']);
 					$('.link').attr('href', data.productMatches['0']['link']).text('Buy Your Own Bottle').attr('target', '_blank');
+
           // - local storage of information given to the user
           localStorage.setItem("paired wines", data.pairedWines['0']);
           localStorage.setItem("pairing text", data.pairingText);
@@ -61,7 +73,8 @@ $(() => {
           localStorage.setItem("link", data.productMatches['0']['link']);
           console.log(localStorage);
 				}
-				$modal.css('display', 'block');// - update display to block AFTER updating the text.
+        // - resets the input box so that the user doesn't have to physically clear it
+				$modal.css('display', 'block');
 			},
 			// - log any errors to the page
 			(error) => {
